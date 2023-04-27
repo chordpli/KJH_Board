a2008fb9db5b860165a672348cc5d48e023ae76e
chordpli <chordplaylist@gmail.com>
2023-04-28 00:08:26 +0900

    build: build project

---
+++ src/main/java/com/kjh/borad/KjhBoradApplication.java
@@ -0,0 +1,13 @@
+package com.kjh.borad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjhBoradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjhBoradApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjh/borad/KjhBoradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjh.borad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjhBoradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
